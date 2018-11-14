42d225629c14542fc657497f1411884404953f73
Alpay <alpaycansener@gmail.com>
2018-11-14 23:34:56 +0300

    Add randomString & mini fixes

--- src/business/orderstate/Saved.java
+++ src/business/orderstate/Saved.java
@@ -2,6 +2,8 @@ package business.orderstate;
 
 import business.Order;
 
+import java.util.Random;
+
 public class Saved extends State {
 
     @Override
@@ -16,6 +18,19 @@ public class Saved extends State {
         }
 
         throw new RuntimeException("undefined transition.");
+    }
+
+    private String randomString(int length) {
+        String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder string = new StringBuilder();
+        Random rnd = new Random();
+
+        while (string.length() < length) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * ALPHABET.length());
+            string.append(ALPHABET.charAt(index));
+        }
+
+        return string.toString();
     }
 
 }
